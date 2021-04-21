61990b7c41bd53b8b04f7a1cd03eedc856349170
shoki_sakai <sakai.shoki.18@shzuoka.ac.jp>
2021-04-21 12:33:04 +0900

    一応動くようになった

--- use_api.py
+++ use_api.py
@@ -4,12 +4,12 @@ import time
 from glob import glob
 from google.cloud import language_v1
 
-# file_path = '../exp_data/tag_cleared_text/believe3/cleared_formatted_condition1_believe3.txt'
-files = glob('../exp_data/tag_cleared_text/**/**.txt')
+dir_name = input('input dir name: ')
+files = glob('../exp_data/tag_cleared_text/' + dir_name + '/**.txt')
+counter = 0
 
 def analyze_text_sentiment(text_content):
     client = language_v1.LanguageServiceClient()
-
     document = {
         'content': text_content,
         'type_': language_v1.Document.Type.PLAIN_TEXT,
@@ -29,21 +29,18 @@ for file_path in files:
 
         name = os.path.basename(os.path.dirname(file_path))
         file_name = os.path.splitext(os.path.basename(file_path))
-        output_path = './sentiment_analyzed/' + name + '/sentiment_' + file_name[0] + '.txt'
+        output_path = './sentiment_analyzed/{}/sentiment_{}.txt'.format(name, file_name[0])
         output_file = open(output_path, 'w', encoding='utf-8')
 
+        print('analzying sentiment...')
         for line_text in text:
             response = analyze_text_sentiment(line_text)
-            # 文ごとの感情分析結果
+            # 文ごとの感情分析結果を出力
             for sentence in response.sentences:
-                output_file.write(u'Sentence text: {}'.format(sentence.text.content) + '\n')
-                output_file.write(u'Sentence sentiment score: {}'.format(sentence.sentiment.score) + '\n')
-                output_file.write(u'Sentence sentiment magnitude: {}'.format(sentence.sentiment.magnitude) + '\n')
+                output_file.write(u'Sentence text: {}\n'.format(sentence.text.content))
+                output_file.write(u'Sentence sentiment score: {}\n'.format(sentence.sentiment.score))
+                output_file.write(u'Sentence sentiment magnitude: {}\n'.format(sentence.sentiment.magnitude))
 
         output_file.close()
-
-        # 文ごとの感情分析結果
-        for sentence in response.sentences:
-            print(u'Sentence text: {}'.format(sentence.text.content))
-            print(u'Sentence sentiment score: {}'.format(sentence.sentiment.score))
-            print(u'Sentence sentiment magnitude: {}'.format(sentence.sentiment.magnitude))
+    counter += 1
+    print('complete %s file' %(counter))
